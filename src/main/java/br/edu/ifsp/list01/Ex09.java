28cdb5ee218352c6e195d8dfeb658777f1cdef1c
Sarah Frossard <sgsfrossard@gmail.com>
2026-03-03 11:10:57 -0300

    Implement age statistics calculation logic (Ex09)

--- src/main/java/br/edu/ifsp/list01/Ex09.java
+++ src/main/java/br/edu/ifsp/list01/Ex09.java
@@ -16,17 +16,61 @@ package br.edu.ifsp.list01;
     | 25 30 45 -1         | 33.33 3 0.00%  |
     => Exercício gentilmente cedido pelos profs. Jorge Cutigi (IFSP/SCL) e Adenilso Simão (ICMC/USP)
 */
+
+import java.util.Scanner;
+
 public class Ex09 {
 
     public static void main(String[] args) {
-        //Leia o input
-        //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
-        //Escreva o resultado da chamada do método compute() aqui
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] input = new int[50];
+        int i = 0;
+
+        for (int idade = scanner.nextInt(); idade >= 0; idade = scanner.nextInt()) {
+            input[i] = idade;
+            i++;
+        }
+
+        Ex09 ex09 = new Ex09();
+
+     
+        System.out.println(ex09.compute(input));
     }
 
     String compute(int[] input) {
-        String output = null;
-        //put your logic here
-        return output;
+
+        int soma = 0;
+        int total = 0;
+        double media;
+        int maioresIdade = 0;
+        int idosos = 0;
+        double porcentagemIdosos;
+
+        for (int i = 0; i < input.length; i++) {
+
+            if (input[i] < 0) {
+                break;
+            }
+
+            soma += input[i];
+            total++;
+
+            if (input[i] >= 18) {
+                maioresIdade++;
+            }
+
+            if (input[i] > 75) {
+                idosos++;
+            }
+        }
+
+        media = (double) soma / total;
+        porcentagemIdosos = (double) idosos * 100 / total;
+
+        return String.format("%.2f %d %.2f%%", media, maioresIdade, porcentagemIdosos);
     }
 }
+
+
